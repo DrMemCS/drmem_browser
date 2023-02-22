5676c741522325a4bd0f0aa21298e7d91d7ef501
Rich Neswold <rich.neswold@gmail.com>
2023-02-21 22:14:41 -0600

    :sparkles: initial GraphQL requests are working
    This version does a query to get all the driver information of a node.
    They are displayed on the node details window. Tapping an entry
    will bring up a window with the descriptive text.

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ This project was built using VSCode.
 
 NOTE: This is a new project and currently doesn't do much. This README will be updated as new features are added.
 
-- Listens to mDNS annoucements for instances of DrMem and adds them to a ListView which some information obtained from the annoucement. Selecting an instance will take you to another screen which does nothing. To get back, you have to restart the app.
+- Listens to mDNS annoucements for instances of DrMem and adds them to a ListView which some information obtained from the annoucement. Selecting an instance will take you to another screen which shows node details.
+  - The details page uses GraphQL to query the node as to what drivers are installed.
+
+Before the project can be built, the GraphQL files need to be processed. This can be done with the command:
+
+```shell
+$ flutter pub run build_runner build --delete-conflicting-outputs
+```
--- lib/node_details.dart
+++ lib/node_details.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'schema/__generated__/device_info.data.gql.dart';
+import 'schema/__generated__/device_info.req.gql.dart';
+import 'package:ferry/ferry.dart';
 import 'package:nsd/nsd.dart';
 import 'mdns_chooser.dart';
 
@@ -17,8 +22,41 @@ class _NodeInfo extends StatefulWidget {
 // we need to make two GraphQL requests to the node.
 
 class _State extends State<_NodeInfo> {
-  final EdgeInsets headerInsets = const EdgeInsets.only(top: 20.0, bottom: 8.0);
+  final EdgeInsets headerInsets =
+      const EdgeInsets.only(top: 20.0, bottom: 8.0, left: 8.0);
   final EdgeInsets all8 = const EdgeInsets.all(8.0);
+
+  List<GAllDriversData_driverInfo>? drivers;
+
+  // Build the URI to the GraphQL query endpoint.
+
+  late final queryLink = HttpLink(Uri(
+          scheme: "http",
+          host: widget.node.host,
+          port: widget.node.port,
+          path: propToString(widget.node, "queries"))
+      .toString());
+
+  final cache = Cache();
+
+  late final client = Client(link: queryLink, cache: cache);
+
+  @override
+  void initState() {
+    super.initState();
+
+    client.request(GAllDriversReq((b) => b)).listen((response) {
+      if (!response.loading) {
+        final List<GAllDriversData_driverInfo> tmp =
+            response.data?.driverInfo.toList() ?? [];
+
+        tmp.sort((a, b) => a.name.compareTo(b.name));
+        setState(() => drivers = tmp);
+      }
+    });
+  }
+
+  // Returns a widget that serves as a section header.
 
   Widget header(BuildContext context, String label) {
     return Padding(
@@ -27,9 +65,8 @@ class _State extends State<_NodeInfo> {
     );
   }
 
-  Widget infoSection(BuildContext context, String label, Widget? child) {
-    return Column(children: [header(context, label)]);
-  }
+  // Returns a widget that renders property information. This consists of a
+  // dimmed label followed by its value in a highlighted color.
 
   Widget buildProperty(BuildContext context, String label, String? value) {
     final ThemeData td = Theme.of(context);
@@ -55,6 +92,23 @@ class _State extends State<_NodeInfo> {
     );
   }
 
+  // Returns a widget that displays the title of the page. The title
+  // includes a gratuitous icon followed by the name of the node.
+
+  Widget titleWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0, left: 8.0),
+      child: Row(children: [
+        const Icon(Icons.developer_board),
+        Container(width: 12.0),
+        Text(widget.node.name ?? "** Unknown Name **",
+            style: Theme.of(context).textTheme.titleLarge),
+      ]),
+    );
+  }
+
+  // Build the widget.
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -62,42 +116,96 @@ class _State extends State<_NodeInfo> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: Row(children: [
-              const Padding(
-                padding: EdgeInsets.only(left: 8.0, right: 16.0),
-                child: Icon(Icons.developer_board),
+          titleWidget(context),
+          Expanded(
+            child: SingleChildScrollView(
+              clipBehavior: Clip.hardEdge,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildProperty(context, "location",
+                      propToString(widget.node, "location")),
+                  buildProperty(context, "address",
+                      "${widget.node.host}:${widget.node.port}"),
+                  header(context, "GraphQL Endpoints"),
+                  buildProperty(
+                      context, "queries", propToString(widget.node, "queries")),
+                  buildProperty(context, "mutations",
+                      propToString(widget.node, "mutations")),
+                  buildProperty(context, "subscriptions",
+                      propToString(widget.node, "subscriptions")),
+                  header(context, "Drivers"),
+                  drivers == null
+                      ? const Center(child: CircularProgressIndicator())
+                      : _DriversListView(drivers: drivers!),
+                  header(context, "Devices"),
+                ],
               ),
-              Text(widget.node.name ?? "** Unknown Name **",
-                  style: Theme.of(context).textTheme.titleLarge),
-            ]),
-          ),
-          Padding(
-            padding: all8,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buildProperty(
-                    context, "location", propToString(widget.node, "location")),
-                buildProperty(context, "address",
-                    "${widget.node.host}:${widget.node.port}"),
-                header(context, "GraphQL Endpoints"),
-                buildProperty(
-                    context, "queries", propToString(widget.node, "queries")),
-                buildProperty(context, "mutations",
-                    propToString(widget.node, "mutations")),
-                buildProperty(context, "subscriptions",
-                    propToString(widget.node, "subscriptions")),
-              ],
             ),
           ),
-          infoSection(context, "Drivers", null),
-          infoSection(context, "Devices", null),
         ],
       ),
     );
+  }
+}
+
+// Local widget which displays a list of drivers.
+
+class _DriversListView extends StatelessWidget {
+  const _DriversListView({
+    Key? key,
+    required this.drivers,
+  }) : super(key: key);
+
+  final List<GAllDriversData_driverInfo> drivers;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        clipBehavior: Clip.hardEdge,
+        shrinkWrap: true,
+        itemCount: drivers.length,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Theme.of(context).cardColor,
+            margin: const EdgeInsets.only(left: 16.0, right: 8.0, bottom: 8.0),
+            child: ListTile(
+                title: Text(drivers[index].name),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                  child: Text(drivers[index].summary),
+                ),
+                trailing: IconButton(
+                    onPressed: () async {
+                      return showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Card(
+                                color: Theme.of(context).cardColor,
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                        child: Markdown(
+                                            data: drivers[index].description)),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ElevatedButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context, false),
+                                          child: const Text("Close")),
+                                    )
+                                  ],
+                                )),
+                          );
+                        },
+                      );
+                    },
+                    icon: const Icon(Icons.help))),
+          );
+        });
   }
 }
 
