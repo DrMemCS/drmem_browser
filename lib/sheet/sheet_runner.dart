db0345a5acd7a4c7d5d2e06bb4cbe8dc820c02d5
Rich Neswold <rich.neswold@gmail.com>
2024-08-29 00:22:33 -0500

    :recycle: make `SheetRunner` a `StatelessWidget`

--- lib/sheet/sheet_runner.dart
+++ lib/sheet/sheet_runner.dart
@@ -8,62 +8,61 @@ import 'package:drmem_browser/sheet/sheet.dart';
 // state is a list of BaseRow types. These rows are organized using a Column
 // widget.
 //
-// Each row type has a run behavior that the derived class has to implement.
-// Comment rows render their content as Markdown. Device rows start monitoring
-// the specified device. If the device is settable, the row allows one to
+// Each row type has a run behavior that the derived class has to implement
+// (comment rows render their content as Markdown; device rows start monitoring
+// the specified device, etc.) If the device is settable, the row allows one to
 // send settings to the device. The Chart row has configuration information to
 // plot historical data for a set of devices.
 
-class SheetRunner extends StatefulWidget {
+class SheetRunner extends StatelessWidget {
   const SheetRunner({super.key});
 
+  // Render the sheet. This consists of building a ListView containing `Card`s
+  // which, in turn, contain a group of widgets. `EmptyRow` rows define the
+  // boundary of groups.
   @override
-  State<SheetRunner> createState() => _SheetRunnerState();
-}
+  Widget build(BuildContext context) =>
+      BlocBuilder<Model, AppState>(builder: (context, state) {
+        final List<Card> groups = state.selected.rows
 
-// Holds the state for a Runner sheet. A runner sheet is a view where the rows
-// are collecting data from DrMem.
+            // Build a list of lists of widgets. We start with a list that has
+            // one empty list. As we iterate across the row types on the page,
+            // we do one of two things:
+            //
+            // 1) If the row is an `EmptyRow`, it signifies a new Card. In this
+            //    case we append an empty list.
+            // 2) Any other row type, we append it to the list at the end of
+            //    the list.
 
-class _SheetRunnerState extends State<SheetRunner> {
-  @override
-  void initState() => super.initState();
+            .fold(
+                [<Widget>[]],
+                (result, e) => switch (e) {
+                      EmptyRow() => result..add([]),
+                      DeviceRow() || PlotRow() || CommentRow() => result
+                        ..last.add(Padding(
+                            key: e.key,
+                            padding: const EdgeInsets.only(bottom: 2.0),
+                            child: e.buildRowRunner(context)))
+                    })
 
-  @override
-  void dispose() => super.dispose();
+            // As we iterate through the list of widget lists, this method will
+            // filter out the empty lists.
 
-  // Render the sheet. This consists of building a ListView containing all
-  // the rows defined for the sheet. We build all the rows and insert them
-  // in the view. This is less efficient, with resources, than using
-  // `ListView.builder()` but we do this so we're not contantly restarting
-  // subscriptions as rows go in and out of the visible part of the list.
+            .where((e) => e.isNotEmpty)
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<Model, AppState>(builder: (context, state) {
-      List<Card> groups = state.selected.rows
-          .fold([<Widget>[]], (result, e) {
-            if (e is EmptyRow) {
-              result.add([]);
-            } else {
-              result.last.add(Padding(
-                  key: e.key,
-                  padding: const EdgeInsets.only(bottom: 2.0),
-                  child: e.buildRowRunner(context)));
-            }
-            return result;
-          })
-          .where((e) => e.isNotEmpty)
-          .map((e) => Card(
-              margin: const EdgeInsets.fromLTRB(0.0, 6.0, 0.0, 0.0),
-              child: Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: Column(mainAxisSize: MainAxisSize.max, children: e),
-              )))
-          .toList();
+            // All we have left are non-empty lists. Treat them as a group and
+            // add them in a `Card`.
 
-      return ListView(
-          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-          children: groups);
-    });
-  }
+            .map((e) => Card(
+                margin: const EdgeInsets.fromLTRB(0.0, 6.0, 0.0, 0.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Column(mainAxisSize: MainAxisSize.max, children: e),
+                )))
+            .toList();
+
+        return ListView(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            children: groups);
+      });
 }
